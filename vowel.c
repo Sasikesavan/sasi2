c05d577f07264de42ab775d721c2d5badccd0e87
Sasikesavan <sasiprabakesavan@gmail.com>
2016-07-10 22:02:08 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("%d",&i);
+switch(i)
+case a:
+printf("the letter is a vowel");
+break;
+case e:
+printf("the letter is a vowel");
+break;
+case i:
+printf("the letter is a vowel");
+break;
+case o:
+printf("the letter is a vowel");
+break;
+case u:
+printf("the letter is a vowel");
+break;
+default:
+printf("the letter is a consonent")'
+break;
+}
